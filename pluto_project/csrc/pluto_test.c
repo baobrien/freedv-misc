d121901f641490385e317e79a19a767b57f27874
Brady O'Brien <baobrien@baobrien.org>
2019-07-13 22:36:55 -0700

    Fix better TX buf handling; Move TX interpolation to TX thread

--- pluto_project/csrc/pluto_test.c
+++ pluto_project/csrc/pluto_test.c
@@ -19,47 +19,44 @@
 #endif
 
 #define R_TO_M 0.0009765625
-#define M_TO_R 16384
+#define M_TO_R 3276
 
 #define IIO_BUF_SIZE 4096
 
-uint64_t GetTimeStamp() {
-    struct timeval tv;
-    gettimeofday(&tv,NULL);
-    return tv.tv_sec*(uint64_t)1000000+tv.tv_usec;
-}
-
-typedef struct _tdma_stuff_holder{
+typedef struct _tdma_burst {
     float complex *     tx_buffer;
     int64_t             tx_time;
-	size_t				buf_samps;
-	struct _tdma_stuff_holder* next;
-} tdma_stuff_holder;
+	size_t				n_tx_samps;
+	struct _tdma_burst* next;
+} tdma_burst;
 
 typedef struct {
-	tdma_stuff_holder* first;
+	tdma_burst *first;
+	pthread_mutex_t *queue_lock;
 } tx_queue;
 
 struct tx_thread_stuff {
 	struct iio_device *tx_dev;
 	int rate_decim;
+	int nout;
+	int rate_bb;
+	float f_shift;
 	tx_queue tx_baseband_queue;
-	pthread_mutex_t *queue_lock;
 };
-
 
 int cb_tx_burst(tdma_t * tdma,float complex* samples, size_t n_samples,i64 timestamp,void * cb_data){
     tx_queue * tx_q = (tx_queue*) cb_data;
 
-	tdma_stuff_holder * tx_stuff = malloc(sizeof(tdma_stuff_holder));
+	tdma_burst * tx_stuff = malloc(sizeof(tdma_burst));
 	tx_stuff->tx_buffer = malloc(sizeof(complex float)*n_samples);
     tx_stuff->tx_time = timestamp;
 	tx_stuff->next = NULL;
-    for(int i = 0; i < n_samples; i++){
-        samples[i] = samples[i]*.2;
-    }
+	tx_stuff->n_tx_samps = n_samples;
+
     memcpy(tx_stuff->tx_buffer,samples,sizeof(float complex)*n_samples);
-	tdma_stuff_holder * p = tx_q->first;
+
+	pthread_mutex_lock(tx_q->queue_lock);
+	tdma_burst * p = tx_q->first;
 	if (p == NULL) {
 		tx_q->first = tx_stuff;
 	} else {
@@ -68,27 +65,21 @@ int cb_tx_burst(tdma_t * tdma,float complex* samples, size_t n_samples,i64 times
 		}
 		p->next = tx_stuff;
 	}
-}
-
-static size_t cbuffercf_free(cbuffercf buf){
-	return cbuffercf_max_size(buf) - cbuffercf_size(buf);
+	pthread_mutex_unlock(tx_q->queue_lock);
+	return 0;
 }
 
 /* Convert Comp. Short. 16 bit samps from the pluto into complex float */
 static void cs16_to_cf32(complex float * restrict out, short * const in, size_t n, float mult) {
 	// the C standard defines complex float as the same as an array of [r,i]
-	float * restrict out_f = (float*)out;
+	float * restrict out_f = (float*) out;
 	for (size_t i = 0; i < n * 2; i++) {
 		out_f[i] = ((float)in[i]) * mult;
 	}
 }
 
 static void cf32_to_cs16(short * const out, complex float * restrict in, size_t n, float mult) {
-<<<<<<< HEAD
-	float * const in_f = (float*)in;
-=======
-	float * const in_f = in;
->>>>>>> 5957116... Some cleanup; tracking down TX issues
+	float * const in_f = (float*) in;
 	for (size_t i = 0; i < n * 2; i++) {
 		out[i] = (short)(in_f[i] * mult);
 	}
@@ -97,17 +88,29 @@ static void cf32_to_cs16(short * const out, complex float * restrict in, size_t 
 // TODO: move TX re-sampling into TX thread to take work off of main thread
 void tx_thread_entry(void *args){
 	int64_t tx_samp_count = 0;
+	float f_shift;
+	int rate_bb;
 	int rate_decim;
 	int err;
+	int nout;
 
 	struct tx_thread_stuff* tts = (struct tx_thread_stuff*) args;
 
 	struct iio_device *tx_dev;
 	struct iio_channel *tx0_i, *tx0_q;
 	struct iio_buffer *txbuf;
-    
+	
 	tx_dev = tts->tx_dev;
 	rate_decim = tts->rate_decim;
+	f_shift = tts->f_shift;
+	rate_bb = tts->rate_bb;
+	nout = tts->nout;
+
+	iirinterp_crcf iir_uc = iirinterp_crcf_create_default(rate_decim, 8);
+    nco_crcf upmixer = nco_crcf_create(LIQUID_NCO);
+
+    nco_crcf_set_phase(upmixer, 0.0f);
+    nco_crcf_set_frequency(upmixer,2.*M_PI*(f_shift/(float)rate_bb));
 
 	tx0_i = iio_device_find_channel(tx_dev, "voltage0", true);
 	tx0_q = iio_device_find_channel(tx_dev, "voltage1", true);
@@ -122,24 +125,32 @@ void tx_thread_entry(void *args){
 	if (err != 0) {
 		printf("ERR iio_buffer_set_blocking_mode: %s\n",strerror(-err));
 	}
+	complex float * tx_lb_buf = (complex float*) malloc(sizeof(complex float) * nout * rate_decim);
+	complex float * tx_bb_buf = (complex float*) malloc(sizeof(complex float) * nout * rate_decim);
 	complex float * burst_buf_ptr;
 	size_t burst_samps;
 	int64_t burst_start;
-	tdma_stuff_holder* current_burst = NULL;
+	tdma_burst* current_burst = NULL;
 	while (true) {
 		// If no burst is pending, check queue and pop one off
 		if	(current_burst == NULL) {
-			pthread_mutex_lock(tts->queue_lock);
+			pthread_mutex_lock(tts->tx_baseband_queue.queue_lock);
 			if (tts->tx_baseband_queue.first != NULL) {
-				tdma_stuff_holder* cur = tts->tx_baseband_queue.first;
+				tdma_burst* cur = tts->tx_baseband_queue.first;
 				tts->tx_baseband_queue.first = cur->next;
 				current_burst = cur;
 			}
-			pthread_mutex_unlock(tts->queue_lock);
+			pthread_mutex_unlock(tts->tx_baseband_queue.queue_lock);
+			
 			if (current_burst != NULL) {
-				burst_samps = current_burst->buf_samps;
-				burst_buf_ptr = current_burst->tx_buffer;
-				burst_start = current_burst->tx_time;
+				burst_samps = current_burst->n_tx_samps * rate_decim;
+				//burst_buf_ptr = current_burst->tx_buffer;
+				burst_start = (current_burst->tx_time - 55) * rate_decim;
+
+				iirinterp_crcf_execute_block(iir_uc, current_burst->tx_buffer,  current_burst->n_tx_samps, tx_lb_buf);
+				nco_crcf_mix_block_up(upmixer, tx_lb_buf, tx_bb_buf, nout * rate_decim);
+				burst_buf_ptr = tx_bb_buf;
+
 			}
 		} 
 		void *p_dat, *p_end;
@@ -164,7 +175,6 @@ void tx_thread_entry(void *args){
 			// This buffer will contain a burst
 			// How many samples from the start of this buffer does the burst start?
 			size_t burst_start_offset = (size_t) (burst_start - tx_samp_count);
-			size_t  burst_read_samps = 0;
 
 			p_dat += (p_inc*burst_start_offset);
 
@@ -180,9 +190,9 @@ void tx_thread_entry(void *args){
 			cf32_to_cs16((short*)p_dat, burst_buf_ptr, subburst_n_samps, M_TO_R);
 
 			// Update burst counters to reflect TX'ed samples
-			burst_start += burst_read_samps;
-			burst_buf_ptr += burst_read_samps;
-			burst_samps -= burst_read_samps;
+			burst_start += subburst_n_samps;
+			burst_buf_ptr += subburst_n_samps;
+			burst_samps -= subburst_n_samps;
 
 			// If there aren't any more burst samps, end this burst
 			if (burst_samps == 0) {
@@ -220,30 +230,20 @@ int main (int argc, char **argv)
     uint64_t rf_bbf = rf_center - (uint64_t)f_shift;
 
 	iirdecim_crcf iir_dc = iirdecim_crcf_create_default(rate_decim, 8);
-	iirinterp_crcf iir_uc = iirinterp_crcf_create_default(rate_decim, 8);
     nco_crcf downmixer = nco_crcf_create(LIQUID_NCO);
-    nco_crcf upmixer = nco_crcf_create(LIQUID_NCO);
     nco_crcf_set_phase(downmixer, 0.0f);
-    nco_crcf_set_phase(upmixer, 0.0f);
     nco_crcf_set_frequency(downmixer,2.*M_PI*(f_shift/(float)rate_bb));
-    nco_crcf_set_frequency(upmixer,2.*M_PI*(f_shift/(float)rate_bb));
 
     struct TDMA_MODE_SETTINGS mode = FREEDV_4800T;
     tdma_t * tdma = tdma_create(mode);
     const int nout = tdma_nout(tdma);
 	const int nin = tdma_nin(tdma);
-    //tdma_set_rx_cb(tdma,cb_rx_frame,NULL);
-    //tdma_set_tx_cb(tdma,cb_tx_frame,NULL);
+
     tdma->tx_multislot_delay = 9;
-	tdma->loop_delay = 0;
 
     tdma_test_framer * ttf = ttf_create(tdma);
 	pthread_t tx_thread;
 	pthread_mutex_t tx_queue_lock;
-
-	tx_queue tx_bursts;
-	tx_bursts.first = NULL;
-    tdma_set_tx_burst_cb(tdma,cb_tx_burst,(void*)&tx_bursts);
 
 	cbuffercf in1_buffer = cbuffercf_create(nin*rate_decim+4096);
      
@@ -280,7 +280,16 @@ int main (int argc, char **argv)
 	tts.tx_dev = tx_dev;
 	tts.rate_decim = rate_decim;
 	tts.tx_baseband_queue.first = NULL;
-	tts.queue_lock = &tx_queue_lock;
+	tts.tx_baseband_queue.queue_lock = &tx_queue_lock;
+	tts.nout = nout;
+	tts.f_shift = f_shift;
+	tts.rate_bb = rate_bb;
+
+
+	tx_queue * tx_bursts = &tts.tx_baseband_queue;
+	// tx_bursts.first = NULL;
+    
+	tdma_set_tx_burst_cb(tdma,cb_tx_burst,(void*)tx_bursts);
 
 	rx0_i = iio_device_find_channel(rx_dev, "voltage0", 0);
 	rx0_q = iio_device_find_channel(rx_dev, "voltage1", 0);
@@ -304,8 +313,6 @@ int main (int argc, char **argv)
 	int loop_iter = 0;
 	uint64_t rx_samp_count = 0;
 
-	complex float *tx_lb_buf = (complex float*) malloc(sizeof(complex float) * nin * rate_decim);
-
     ttf->print_enable = true;
 	ttf->tx_enable = true;
 	ttf->tx_master = false;
@@ -321,8 +328,7 @@ int main (int argc, char **argv)
 	while (true) {
 		run = true;
 		while(run) {
-			void *p_dat, *p_end, *t_dat;
-			ptrdiff_t p_inc;
+			void *p_dat;
 
 			// Do we have enough room in the output cbuf for a full chunk of samples?
 			if(cbuffercf_max_size(in1_buffer) - cbuffercf_size(in1_buffer) < IIO_BUF_SIZE) {
@@ -338,11 +344,8 @@ int main (int argc, char **argv)
 			if(ret < 0) {
 				printf("err iio_buffer_refill: %s\n", strerror(-ret));
 			}
-			p_inc = iio_buffer_step(rxbuf);
-			p_end = iio_buffer_end(rxbuf);
 			p_dat = iio_buffer_first(rxbuf, rx0_i);
 
-			
 			cs16_to_cf32(cfibuff, p_dat, IIO_BUF_SIZE, R_TO_M);
 			cbuffercf_write(in1_buffer, cfibuff, IIO_BUF_SIZE);	
 			loop_iter++;
@@ -377,44 +380,10 @@ int main (int argc, char **argv)
 
 		}
 
-		while (tx_bursts.first != NULL) {
-			tdma_stuff_holder * tx_stuff = tx_bursts.first;
-			tx_bursts.first = tx_stuff->next;
-
-			tdma_stuff_holder * bb_stuff = (tdma_stuff_holder*) malloc(sizeof(tdma_stuff_holder));
-			bb_stuff->tx_buffer = (complex float*) malloc(sizeof(complex float)*nout*rate_decim);
-
-			iirinterp_crcf_execute_block(iir_uc, tx_stuff->tx_buffer, nout, tx_lb_buf);
-			nco_crcf_mix_block_up(upmixer, tx_lb_buf, bb_stuff->tx_buffer, nout * rate_decim);
-
-			// convert burst time to baseband sample rate and adjust timing for rx->tx latency
-			// 55 samps at 48Khz mesasured on a couple of plutos at a 48000/20 granularity
-			int64_t tx_burst_time = (tx_stuff->tx_time - 55) * rate_decim;
-			
-			size_t tx_burst_n = nout * rate_decim;
-
-			bb_stuff->tx_time = tx_burst_time;
-			bb_stuff->buf_samps = tx_burst_n;
-			bb_stuff->next = NULL;
-
-			free(tx_stuff->tx_buffer);
-			free(tx_stuff);
-
-			pthread_mutex_lock(tts.queue_lock);
-			tdma_stuff_holder *cur = tts.tx_baseband_queue.first;
-			if(cur == NULL) {
-				tts.tx_baseband_queue.first = bb_stuff;
-			} else {
-				while (cur->next != NULL) {
-					cur = cur->next;
-				}
-				cur->next = bb_stuff;
-			}
-			pthread_mutex_unlock(tts.queue_lock);
-
-		}
 		
 	} 
+
+
     // firdecim_crcf_destroy(fir_dc);
     // firinterp_crcf_destroy(fir_uc);
 	iio_buffer_destroy(rxbuf);
